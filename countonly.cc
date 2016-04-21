7de277d2ab7424e37a52c88016c7a40a356e2e9c
Kolya Matteo <kundor@users.noreply.github.com>
2016-04-21 14:43:03 -0400

    Fix threering() and silence "comparison between signed and unsigned integer expressions" warnings

--- countonly.cc
+++ countonly.cc
@@ -11,20 +11,20 @@
 using std::printf;
 using std::putchar;
 
-void onering(int maxnode) {
+void onering(unsigned maxnode) {
     /* Header line */
     printf(" n");
-    for (int i = 0; i < maxnode; ++i) {
+    for (unsigned i = 0; i < maxnode; ++i) {
         if (i < 10)
             putchar(' ');
         printf(" t_%d", i);
     }
     putchar('\n');
     /* number of orbits */
-    for (int numnode = 1; numnode <= maxnode; ++numnode) {
+    for (unsigned numnode = 1; numnode <= maxnode; ++numnode) {
         CoxeterGraph tcg = coxeterA(numnode);
         printf("%2d", numnode);
-        for (int i = 0; i < numnode; ++i) {
+        for (unsigned i = 0; i < numnode; ++i) {
             for (unsigned v = 0; v < numnode; ++v) {
                 if (v == i)
                     tcg[v].ringed = true;
@@ -38,10 +38,11 @@ void onering(int maxnode) {
     }
 }
 
-void tworing(int maxnode, int gap) {
+void tworing(unsigned maxnode, unsigned gap) {
+    if (maxnode < gap) return;
     /* Header line */
     printf(" n");
-    for (int i = 0; i < maxnode - gap; ++i) {
+    for (unsigned i = 0; i < maxnode - gap; ++i) {
         if (i < 10 - gap)
             putchar(' ');
         if (i < 10)
@@ -50,10 +51,10 @@ void tworing(int maxnode, int gap) {
     }
     putchar('\n');
     /* number of orbits */
-    for (int numnode = gap + 1; numnode <= maxnode; ++numnode) {
+    for (unsigned numnode = gap + 1; numnode <= maxnode; ++numnode) {
         CoxeterGraph tcg = coxeterA(numnode);
         printf("%2d", numnode);
-        for (int i = 0; i < numnode - gap; ++i) {
+        for (unsigned i = 0; i < numnode - gap; ++i) {
             for (unsigned v = 0; v < numnode; ++v) {
                 if (v == i || v == i + gap)
                     tcg[v].ringed = true;
@@ -67,30 +68,33 @@ void tworing(int maxnode, int gap) {
     }
 }
 
-void threering(int maxnode, int gap) {
+void threering(unsigned maxnode, unsigned gap=1) {
+    if (maxnode < 2*gap) return;
     /* Header line */
     printf(" n");
-    for (int i = 0; i < maxnode - gap; ++i) {
+    for (unsigned i = 0; i < maxnode - 2*gap; ++i) {
+        if (i < 10 - 2*gap)
+            putchar(' ');
         if (i < 10 - gap)
             putchar(' ');
         if (i < 10)
             putchar(' ');
-        printf(" t_%d,%d", i, i + gap);
+        printf(" t_%d,%d,%d", i, i + gap, i + 2*gap);
     }
     putchar('\n');
     /* number of orbits */
-    for (int numnode = gap + 1; numnode <= maxnode; ++numnode) {
+    for (unsigned numnode = 2*gap + 1; numnode <= maxnode; ++numnode) {
         CoxeterGraph tcg = coxeterA(numnode);
         printf("%2d", numnode);
-        for (int i = 0; i < numnode - gap; ++i) {
+        for (unsigned i = 0; i < numnode - 2*gap; ++i) {
             for (unsigned v = 0; v < numnode; ++v) {
-                if (v == i || v == i + gap)
+                if (v == i || v == i + gap || v == i + 2*gap)
                     tcg[v].ringed = true;
                 else
                     tcg[v].ringed = false;
             }
             FaceOrbitPoset hasse{tcg};
-            printf("%8d", hasse.head->numpaths());
+            printf("%11d", hasse.head->numpaths());
         }
         putchar('\n');
     }
@@ -105,5 +109,7 @@ int main() {
     tworing(maxnode, 2);
     putchar('\n');
     tworing(maxnode, 3);
+    putchar('\n');
+    threering(maxnode);
     return 0;
 }
